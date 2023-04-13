2b5126db6061860e56c9b978e1c3728c374111a5
Mikkel Thygesen <>
2023-04-14 00:37:27 +0200

    implemented number formatting

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,10 @@
+import 'package:intl/intl.dart';
+
+var formatter2dec = NumberFormat('#,###.##');
+var formatter0dec = NumberFormat('#,###');
+
 extension NumExt on num {
-  String fmt2dec() => remainder(1) != 0 ? toStringAsFixed(2) : toInt().toString();
+  String fmt2dec() => remainder(1) != 0
+      ? formatter2dec.format(this)
+      : formatter0dec.format(this);
 }
