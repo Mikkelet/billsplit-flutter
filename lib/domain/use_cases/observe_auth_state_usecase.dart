1a9ca81ab473b7cae47cd684e325a66621d59958
Mikkel Thygesen <>
2023-05-23 11:54:40 +0300

    fixed a few bugs

--- lib/domain/use_cases/observe_auth_state_usecase.dart
+++ lib/domain/use_cases/observe_auth_state_usecase.dart
@@ -4,7 +4,13 @@ import 'package:billsplit_flutter/di/get_it.dart';
 class ObserveAuthStateUseCase {
   final _authProvider = getIt<AuthProvider>();
 
-  Stream<String?> observe(){
-    return _authProvider.authListener();
+  Stream<String?> observe() {
+    return _authProvider.authListener().asyncMap((event) async {
+      if (event == null) {
+        await Future.delayed(const Duration(seconds: 1));
+        return event;
+      }
+      return event;
+    });
   }
 }
