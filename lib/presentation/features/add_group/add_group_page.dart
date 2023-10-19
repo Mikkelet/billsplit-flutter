f2dab4eda0a44baf4fb5c97538d2ddc145b89fc0
MikkelThygesen <Mikkelet@gmail.com>
2023-10-19 15:18:32 +0700

    27: added gradient background, smoothed corners

--- lib/presentation/features/add_group/add_group_page.dart
+++ lib/presentation/features/add_group/add_group_page.dart
@@ -1,7 +1,7 @@
 import 'package:billsplit_flutter/domain/models/currency.dart';
 import 'package:billsplit_flutter/presentation/common/base_bloc_builder.dart';
+import 'package:billsplit_flutter/presentation/common/base_scaffold.dart';
 import 'package:billsplit_flutter/presentation/common/clickable_list_item.dart';
-import 'package:billsplit_flutter/presentation/common/gradient_background.dart';
 import 'package:billsplit_flutter/presentation/dialogs/currency_picker/currency_picker_dialog.dart';
 import 'package:billsplit_flutter/presentation/features/add_group/widgets/add_people_to_group_view.dart';
 import 'package:billsplit_flutter/presentation/features/add_group/widgets/added_person_view.dart';
@@ -33,107 +33,104 @@ class AddGroupPage extends StatelessWidget {
       },
       child: BaseBlocBuilder<AddGroupCubit>(
         builder: (cubit, state) {
-          return GradientBackground(
-            child: Scaffold(
-              appBar: AppBar(
-                forceMaterialTransparency: true,
-                leading: const BackButton(),
-                actions: [
-                  IconButton(
-                      onPressed: () {
-                        cubit.addGroup();
-                      },
-                      icon: const Icon(Icons.check))
-                ],
-              ),
-              backgroundColor: Colors.transparent,
-              body: Builder(builder: (context) {
-                if (state is Loading) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                return SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 32),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            cubit.groupName.isEmpty
-                                ? "New group"
-                                : cubit.groupName,
-                            style: Theme.of(context).textTheme.displaySmall,
-                          ),
+          return BaseScaffold(
+            appBar: AppBar(
+              forceMaterialTransparency: true,
+              leading: const BackButton(),
+              actions: [
+                IconButton(
+                    onPressed: () {
+                      cubit.addGroup();
+                    },
+                    icon: const Icon(Icons.check))
+              ],
+            ),
+            body: Builder(builder: (context) {
+              if (state is Loading) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 32),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          cubit.groupName.isEmpty
+                              ? "New group"
+                              : cubit.groupName,
+                          style: Theme.of(context).textTheme.displaySmall,
                         ),
-                        const SizedBox(height: 16),
-                        RoundedListItem(
-                            child: TextField(
-                              autofocus: cubit.groupName.isEmpty,
-                              maxLines: 1,
-                              maxLength: 40,
-                              controller: nameTextController,
-                              onChanged: (value) {
-                                cubit.onUpdateGroupName(value);
-                              },
-                              textInputAction: TextInputAction.done,
-                              style: SplitsbyTextTheme.textFieldStyle(context),
-                              decoration: InputDecoration(
-                                  hintStyle: SplitsbyTextTheme.textFieldHintStyle(
-                                      context),
-                                  counterText: "",
-                                  border: InputBorder.none,
-                                  hintText: "Enter group name"),
-                            )),
-                        const SizedBox(height: 16),
-                        ClickableListItem(
-                          padding: const EdgeInsets.all(16),
-                          onClick: () async {
-                            final response = await Navigator.of(context)
-                                .push(CurrencyPickerDialog.getRoute());
-                            if (response is Currency) {
-                              cubit.updateCurrency(response);
-                            }
-                          },
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                cubit.currency.toUpperCase(),
-                                style: Theme.of(context).textTheme.labelLarge,
-                              ),
-                              const Icon(Icons.arrow_drop_down)
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 18),
-                        Column(
+                      ),
+                      const SizedBox(height: 16),
+                      RoundedListItem(
+                          child: TextField(
+                            autofocus: cubit.groupName.isEmpty,
+                            maxLines: 1,
+                            maxLength: 40,
+                            controller: nameTextController,
+                            onChanged: (value) {
+                              cubit.onUpdateGroupName(value);
+                            },
+                            textInputAction: TextInputAction.done,
+                            style: SplitsbyTextTheme.textFieldStyle(context),
+                            decoration: InputDecoration(
+                                hintStyle: SplitsbyTextTheme.textFieldHintStyle(
+                                    context),
+                                counterText: "",
+                                border: InputBorder.none,
+                                hintText: "Enter group name"),
+                          )),
+                      const SizedBox(height: 16),
+                      ClickableListItem(
+                        padding: const EdgeInsets.all(16),
+                        onClick: () async {
+                          final response = await Navigator.of(context)
+                              .push(CurrencyPickerDialog.getRoute());
+                          if (response is Currency) {
+                            cubit.updateCurrency(response);
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            RoundedListItem(
-                              child: Column(
-                                children: [
-                                  if (cubit.people.isEmpty)
-                                    const Text("Add people to the group")
-                                  else
-                                    ...cubit.people
-                                        .map((e) => AddedPersonView(person: e))
-                                        .toList(),
-                                ],
-                              ),
+                            Text(
+                              cubit.currency.toUpperCase(),
+                              style: Theme.of(context).textTheme.labelLarge,
                             ),
-                            const SizedBox(height: 8),
-                            const Align(
-                              alignment: Alignment.centerRight,
-                              child: AddPeopleToGroupView(),
-                            )
+                            const Icon(Icons.arrow_drop_down)
                           ],
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 18),
+                      Column(
+                        children: [
+                          RoundedListItem(
+                            child: Column(
+                              children: [
+                                if (cubit.people.isEmpty)
+                                  const Text("Add people to the group")
+                                else
+                                  ...cubit.people
+                                      .map((e) => AddedPersonView(person: e))
+                                      .toList(),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          const Align(
+                            alignment: Alignment.centerRight,
+                            child: AddPeopleToGroupView(),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                );
-              }),
-            ),
+                ),
+              );
+            }),
           );
         },
       ),
