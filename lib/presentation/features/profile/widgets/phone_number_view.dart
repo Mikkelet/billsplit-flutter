fb5945e637e09b898ca6b1f3f7bb6c440f92b849
Mikkel Thygesen <mikkelet@gmail.com>
2024-01-21 20:19:19 +0100

    changed profile menu to look more menu-ish

--- lib/presentation/features/profile/widgets/phone_number_view.dart
+++ lib/presentation/features/profile/widgets/phone_number_view.dart
@@ -1,5 +1,5 @@
 import 'package:billsplit_flutter/presentation/base/bloc/base_cubit.dart';
-import 'package:billsplit_flutter/presentation/common/clickable_list_item.dart';
+import 'package:billsplit_flutter/presentation/features/profile/widgets/profile_list_item.dart';
 import 'package:billsplit_flutter/presentation/features/update_phone_number/update_phone_number_flow.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,26 +11,23 @@ class PhoneNumberView<T extends BaseCubit> extends StatelessWidget {
   Widget build(BuildContext context) {
     final cubit = context.read<T>();
     final phoneNumber = cubit.user.phoneNumberState;
+
     return Builder(
       builder: (context) {
         if (phoneNumber.phoneNumber.isEmpty) {
-          return TextButton(
-            onPressed: () async {
+          return ProfileListItem(
+            text: "Click here to add phone number",
+            icon: Icons.mode_edit_outlined,
+            onClick: () async {
               await Navigator.of(context)
                   .push(UpdatePhoneNumberFlow.getRoute());
               cubit.update();
             },
-            child: const Text("Click here to add phone number"),
           );
         }
-        return ClickableListItem(
-          alignment: Alignment.centerLeft,
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          height: 64,
-          child: Text(
-            phoneNumber.display,
-            style: Theme.of(context).textTheme.labelLarge,
-          ),
+        return ProfileListItem(
+          text: phoneNumber.display,
+          icon: Icons.mode_edit_outlined,
           onClick: () async {
             await Navigator.of(context)
                 .push(UpdatePhoneNumberFlow.getRoute(phoneNumber: phoneNumber));
