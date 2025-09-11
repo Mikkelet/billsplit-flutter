ddd5dc96600203eee2708e9b1f4d0eae8dbd2e27
MikkelThygesen <mikkel@optikosprime.com>
2025-09-11 21:31:26 +0200

    updated pipeline

--- lib/domain/use_cases/profile/parse_phonenumber_usecase.dart
+++ lib/domain/use_cases/profile/parse_phonenumber_usecase.dart
@@ -1,6 +1,6 @@
 import 'package:billsplit_flutter/domain/models/phone_number.dart';
 import 'package:flutter/services.dart';
-import 'package:phone_number/phone_number.dart' as phone_num_parser;
+import 'package:phone_numbers_parser/phone_numbers_parser.dart' as pnp;
 
 class ParsePhoneNumberUseCase {
 
@@ -9,10 +9,10 @@ class ParsePhoneNumberUseCase {
     if(phoneNumber == null) return null;
     if (phoneNumber.isEmpty) return null;
     try {
-      final parse = await phone_num_parser.PhoneNumberUtil().parse(phoneNumber);
+      final parse = pnp.PhoneNumber.parse(phoneNumber);
       final countryCode = parse.countryCode.startsWith("+") ? parse.countryCode : "+${parse.countryCode}";
       return PhoneNumber(
-        phoneNumber: parse.nationalNumber,
+        phoneNumber: parse.nsn,
         countryCode: countryCode,
       );
     } on PlatformException {
