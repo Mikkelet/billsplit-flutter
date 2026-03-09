2fe2439b097ab3cc996d8b923908d98c444afe00
MikkelThygesen <mikkel@trackntake.com>
2026-03-10 00:09:22 +0100

    updated pipeline

--- .github/workflows/scripts/install_ios_cert.sh
+++ .github/workflows/scripts/install_ios_cert.sh
@@ -2,6 +2,6 @@ P12_PASSWORD=$1
 KEYCHAIN_PATH=$2
 KEYCHAIN_PASSWORD=$3
 
-security import $CERTIFICATE_PATH -P "$P12_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
-security set-key-partition-list -S apple-tool:,apple: -k "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
+security import $CERTIFICATE_PATH -P $P12_PASSWORD -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+security set-key-partition-list -S apple-tool:,apple: -k $KEYCHAIN_PASSWORD $KEYCHAIN_PATH
 security list-keychain -d user -s $KEYCHAIN_PATH
